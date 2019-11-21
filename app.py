aff6f06a07fd790cbeb2e83f9c992439c414279e
Anthony <sean.a.protho1@gmail.com>
2019-11-20 16:16:21 -0800

    jinja templated 8D

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ app = Flask(__name__, static_url_path="")
 
 @app.route("/")
 def index():
-    template = env.get_template('index.html')
-    return render_template(template)
+    #template = env.get_template()
+    return render_template('index.html')
 
 @app.route("/music")
 def music():
-    template = env.get_template('music.html')
-    return render_template(template)
+    #template = env.get_template()
+    return render_template('music.html')
 
 if __name__ == '__main__':
     app.run(debugger=True)
